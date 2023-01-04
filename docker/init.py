faf46a12a5c19c0a2fe0b110872920c1272b4138
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2023-01-04 12:21:06 +0100

    new_cluster: set coiled env from image specifier
    Previously, the default value of the coiled environment (i.e.
    "software" parameter) was hard-coded. Now it is set according
    to the current Jupyter user image.

--- docker/init.py
+++ docker/init.py
@@ -1,57 +1,56 @@
 import os
+import re
 from xcube.core.store import new_data_store
 import distributed.deploy
 from typing import Optional
 
 max_depth = 8
 
-lab_store = new_data_store(
-    "file",
-    root="/home/jovyan/"
-)
+lab_store = new_data_store("file", root="/home/jovyan/")
 user_store = new_data_store(
     "s3",
     root=f"agriculture-vlab-user/{os.environ['JUPYTERHUB_USER']}/",
-    max_depth=max_depth
+    max_depth=max_depth,
 )
 scratch_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-scratch/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-scratch/", max_depth=max_depth
 )
 test_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data-test",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data-test", max_depth=max_depth
 )
 staging_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data-staging/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data-staging/", max_depth=max_depth
 )
 data_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data/", max_depth=max_depth
 )
 public_store_write = new_data_store(
     "s3",
     root=f"agriculture-vlab-public/{os.environ['JUPYTERHUB_USER']}/",
-    max_depth=max_depth
+    max_depth=max_depth,
 )
 public_store_read = new_data_store(
-    "s3",
-    root=f"agriculture-vlab-public/",
-    max_depth=max_depth
+    "s3", root=f"agriculture-vlab-public/", max_depth=max_depth
 )
 
-def new_cluster(provider: str = 'coiled',
-                name: Optional[str] = None,
-                software: str = 'avl-user-1-2-0',
-                n_workers: int = 4,
-                **kwargs) -> distributed.deploy.Cluster:
+
+def new_cluster(
+    provider: str = 'coiled',
+    name: Optional[str] = None,
+    software: str = None,
+    n_workers: int = 4,
+    **kwargs,
+) -> distributed.deploy.Cluster:
+    if software is None:
+        # Construct an identifier from the current user image specifier.
+        software = re.sub(
+            '[:.]',
+            '-',
+            re.search(r'/([^/]+)$', os.environ['JUPYTER_IMAGE']).group(1),
+        )
     if provider == 'coiled':
         from coiled import Cluster
+
         return Cluster(
             # No config (see ~/.config/dask/coiled.yml)
             n_workers=n_workers,
@@ -68,6 +67,6 @@ def new_cluster(provider: str = 'coiled',
             name=name,  # config 'coiled.name'
             software=software,  # config 'coiled.software'
             # Other
-            **kwargs
+            **kwargs,
         )
     raise NotImplementedError(f'Unknown provider {provider!r}')
