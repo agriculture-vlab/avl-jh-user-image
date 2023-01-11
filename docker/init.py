bdbbf995568e352381724448b48289da0ec5704b
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2023-01-11 15:12:05 +0100

    Update init file

--- docker/init.py
+++ docker/init.py
@@ -1,58 +1,67 @@
 import os
+import re
 from xcube.core.store import new_data_store
 import distributed.deploy
 from typing import Optional
 
 max_depth = 8
 
-lab_store = new_data_store(
-    "file",
-    root="/home/jovyan/"
-)
+lab_store = new_data_store("file", root="/home/jovyan/")
 user_store = new_data_store(
     "s3",
     root=f"agriculture-vlab-user/{os.environ['JUPYTERHUB_USER']}/",
-    max_depth=max_depth
+    max_depth=max_depth,
 )
 scratch_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-scratch/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-scratch/", max_depth=max_depth
 )
 test_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data-test",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data-test", max_depth=max_depth
 )
 staging_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data-staging/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data-staging/", max_depth=max_depth
 )
 data_store = new_data_store(
-    "s3",
-    root="agriculture-vlab-data/",
-    max_depth=max_depth
+    "s3", root="agriculture-vlab-data/", max_depth=max_depth
 )
 public_store_write = new_data_store(
     "s3",
     root=f"agriculture-vlab-public/{os.environ['JUPYTERHUB_USER']}/",
-    max_depth=max_depth
+    max_depth=max_depth,
 )
 public_store_read = new_data_store(
-    "s3",
-    root=f"agriculture-vlab-public/",
-    max_depth=max_depth
+    "s3", root=f"agriculture-vlab-public/", max_depth=max_depth
 )
 
-def new_cluster(provider: str = 'coiled',
-                name: Optional[str] = None,
-                software: str = 'avl-user-1-2-0',
-                n_workers: int = 4,
-                **kwargs) -> distributed.deploy.Cluster:
+
+def new_cluster(
+    provider: str = 'coiled',
+    name: Optional[str] = None,
+    software: str = None,
+    n_workers: int = 4,
+    **kwargs,
+) -> distributed.deploy.Cluster:
+    account = 'bc'
+
     if provider == 'coiled':
-        from coiled import Cluster
-        return Cluster(
+        import coiled
+        if software is None:
+            # Construct an identifier from the current user image specifier.
+            current_image = os.environ['JUPYTER_IMAGE']
+            software = re.sub(
+                '[:.]',
+                '-',
+                re.search(r'/([^/]+)$', current_image).group(1),
+            )
+            # If it doesn't exist yet, create it
+            available_environments = coiled.list_software_environments(account=account).keys()
+            if software not in available_environments:
+                coiled.create_software_environment(
+                    name=software,
+                    container=current_image
+                )
+        
+        return coiled.Cluster(
             # No config (see ~/.config/dask/coiled.yml)
             n_workers=n_workers,
             environ=None,  # Pass credentials to workers?
@@ -63,11 +72,11 @@ def new_cluster(provider: str = 'coiled',
                 'purpose': 'AVL dask cluster',
             },
             # From login
-            account='bc',
+            account=account,
             # From config
             name=name,  # config 'coiled.name'
             software=software,  # config 'coiled.software'
             # Other
-            **kwargs
+            **kwargs,
         )
     raise NotImplementedError(f'Unknown provider {provider!r}')
